fcaa0726bd18ac488da88821b4453cfdbf00574f
Asim0096 <Asim0096@gmail.com>
2025-05-08 00:21:16 +0300

    add addation things in game file

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
 let player = {
   x: 50,
   y: 200,
@@ -8,6 +11,20 @@ let player = {
   grounded: false
 };
 
+let gravity = 0.5;
+let obstacles = [];
+
+function applyGravity() {
+  player.velocityY += gravity;
+  player.y += player.velocityY;
+
+  if (player.y + player.height >= canvas.height) {
+    player.y = canvas.height - player.height;
+    player.velocityY = 0;
+    player.grounded = true;
+  }
+}
+
 document.addEventListener("keydown", (e) => {
   if (e.code === "Space" && player.grounded) {
     player.velocityY = player.jumpForce;
@@ -15,35 +32,41 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-function applyGravity() {
-  player.velocityY += 0.5; 
-  player.y += player.velocityY;
-
-  if (player.y + player.height >= 400) { 
-    player.y = 400 - player.height;
-    player.velocityY = 0;
-    player.grounded = true;
-  }
-}
-let obstacles = [];
-
 function generateObstacle() {
   obstacles.push({
-    x: 800,
-    y: 350,
+    x: canvas.width,
+    y: canvas.height - 50,
     width: 50,
     height: 50
   });
 }
 
-setInterval(generateObstacle, 2000); 
-
 function updateObstacles() {
   for (let i = 0; i < obstacles.length; i++) {
-    obstacles[i].x -= 5; 
+    obstacles[i].x -= 5;
   }
 }
 
-applyGravity();
-updateObstacles();
+function drawPlayer() {
+  ctx.fillStyle = "#0077ff";
+  ctx.fillRect(player.x, player.y, player.width, player.height);
+}
 
+function drawObstacles() {
+  ctx.fillStyle = "#ff4444";
+  obstacles.forEach(ob => {
+    ctx.fillRect(ob.x, ob.y, ob.width, ob.height);
+  });
+}
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  applyGravity();
+  updateObstacles();
+  drawPlayer();
+  drawObstacles();
+  requestAnimationFrame(gameLoop);
+}
+
+setInterval(generateObstacle, 2000);
+gameLoop();
